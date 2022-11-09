aa57b0626083c522b11cff3c221d5eba2991dd51
gab-che <gabriellachessa1@gmail.com>
2022-11-09 16:04:12 +0100

    button click generates grid

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const gridGenerator = document.getElementById("grid_generator");
 gridGenerator.addEventListener("click", function(){
 
     const difficulty = document.querySelector("[name='difficulty']");
-    const difficultyValue = difficulty.value;
+    const difficultyValue = parseInt(difficulty.value);
     
     createGrid(difficultyValue);
 })
@@ -15,4 +15,12 @@ gridGenerator.addEventListener("click", function(){
  */
 function createGrid(difficultyValue){
     const gridContainer = document.querySelector(".grid_container");
+    gridContainer.innerHTML = "";
+    
+    for (i = 0; i < difficultyValue; i++){
+        const newCell = document.createElement("div");
+        newCell.classList.add("grid_cell");
+        newCell.style.width = `calc(100% / ${Math.sqrt(difficultyValue)})`;
+        gridContainer.append(newCell);
+    }
 }
